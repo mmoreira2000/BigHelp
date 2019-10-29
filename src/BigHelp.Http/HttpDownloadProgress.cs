855cb89d3c5f9808c499bd563cf5e5c8fd8d30e6
Matheus Moreira <matheus.moreira@gmail.com>
2019-10-29 20:08:58 -0300

    Added an extension method "CloneIfAlreadySent" that clones and returns a HttpRequestMessage that was already sent. If the request message was not sent yet, returns itself.

---
+++ src/BigHelp.Http/HttpDownloadProgress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BigHelp.Http
+{
+    public class HttpDownloadProgress
+    {
+        public long? TotalSize { get; internal set; }
+        public long TotalDownloaded { get; internal set; }
+
+        public double PercentDownloaded => TotalSize.HasValue ? Math.Round((double)TotalDownloaded / TotalSize.Value, 4) : -1D;
+
+        internal HttpDownloadProgress()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            var format = "###,###,###,###,###,###,###";
+            if (TotalSize.HasValue)
+            {
+                return $"{TotalDownloaded.ToString(format)}/{TotalSize.Value.ToString(format)} bytes downloaded ({Math.Round(PercentDownloaded * 100, 2)}%)";
+            }
+            return $"{TotalDownloaded.ToString(format)} bytes downloaded";
+        }
+    }
+}
